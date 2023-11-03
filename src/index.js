9e8fc2ba5779b9429723db0180f97bb7ff69bd5f
siddh-kivtechs <siddhbose@kneoginigmisarch.co.in>
2023-11-04 01:48:19 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,20 +15,12 @@ const auth = "authentication";
 const supabaseUri = process.env.SUPABASE_URI;
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUri, supabaseKey);
-
-
-function baseHandler(req, res) {
-  const requestBody = req.body;
-  const message = {};
-  let decodedEmail;
-
-  if (requestBody["email"]) {
-    decodedEmail = CryptoJS.enc.Base64.parse(requestBody["email"]).toString(CryptoJS.enc.Utf8);
-    console.log(`Decoded Email: ${decodedEmail}`);
-  }
-
+async verify_user(data)
+{
+    const message = {};
   // Check if the email and password are both present.
-  if (requestBody["email"] && requestBody["password"]) {
+  if (requestBody["email"] && requestBody["password"]) 
+  {
     // Check if the email is admin or user.
     if (decodedEmail === "admin@kivtechs.cloud") {
       message.auth = "auth success";
@@ -54,11 +46,43 @@ function baseHandler(req, res) {
       return;
     }
   }
+}
 
   // If the email or password is missing, send an auth failed response.
   message.auth = "auth failed";
   message.nextUri = "#";
   res.json(message);
+}
+async decrypt_email(data)
+{
+if (data["email"]) 
+   {
+    decodedEmail = CryptoJS.enc.Base64.parse(requestBody["email"]).toString(CryptoJS.enc.Utf8);
+    console.log(`Decoded Email: ${decodedEmail}`);
+     return decodedEmail;
+  }
+}
+async function verify_data(data)
+{
+   // If the email or password is not present or undefined, do nothing.
+  if (!data["email"] || !data["password"]) {
+    return false;
+  }
+  else 
+  {
+      let decodedEmail=decrypt_email(data);
+   return verify_user(decodedEmail);
+    
+  }
+}
+
+async function baseHandler(req, res) {
+  const requestBody = req.body;
+
+  verify_data(requestBody);
+
+
+
 }
 
 
