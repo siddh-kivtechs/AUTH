d3668a33b586e6726501f10fbfdc75e84ae12cbf
siddh-kivtechs <siddhbose@kneoginigmisarch.co.in>
2023-11-04 01:27:03 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -18,10 +18,53 @@ const supabase = createClient(supabaseUri, supabaseKey);
 
 
 async function baseHandler(req, res)
-  
+  {
+    let requestBody = req.body;
+    let message = {};
+    let decoded_email;
+    if (requestBody["email"]) 
 {
+  decoded_email = CryptoJS.enc.Base64.parse(requestBody["email"]).toString(CryptoJS.enc.Utf8);
+    console.log('Decoded Email:'+decoded_email);
+if (requestBody["email"] && requestBody["password"]   && decoded_email === "admin@kivtechs.cloud") {
+  message.auth = "auth success";
+  message.next_uri = "https://kivtechs.cloud/dashboard/admin";
+    res.json(message);
+} 
+    else if (requestBody["email"] && requestBody["password"] && decoded_email === "kaushik@kivtechs.cloud") {
+  message.auth = "auth success";
+  message.next_uri = "https://kivtechs.cloud/dashboard/kaushik";
+    console.log('Kaushik Login');
+ 
+} 
+  else if (requestBody["email"] && requestBody["password"] && decoded_email === "sohini@kivtechs.cloud") {
+  message.auth = "auth success";
+  message.next_uri = "https://kivtechs.cloud/dashboard/sohini";
+
+        res.json(message);
+} 
   
-  res.send({auth:"auth success",next_uri: "https://kivtechs.cloud/dashboard/user"});
+  else if (requestBody["email"] && requestBody["password"]) {
+  message.auth = "auth success";
+  message.next_uri = "https://kivtechs.cloud/dashboard/user";
+
+    res.json(message);
+} 
+
+  else {
+  message.auth = "auth failed";
+  message.next_uri = "#";
+   res.json(message);
+}
+}
+else {
+  message.auth = "auth failed";
+  message.next_uri = "#";
+  res.json(message);
+}
+
+    
+  // res.send({auth:"auth success",next_uri: "https://kivtechs.cloud/dashboard/user"});
 });
 
 
