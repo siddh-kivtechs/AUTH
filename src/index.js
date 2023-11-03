66771166fe9a2b7394e4c00014e5f1490654abaf
siddh-kivtechs <siddhbose@kneoginigmisarch.co.in>
2023-11-04 01:21:42 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,19 +5,30 @@ import { createClient } from "@supabase/supabase-js";
 import { ulid } from 'ulid';
 import CryptoJS from 'crypto-js';
 
-
-
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(cors());
-app.all("*", (req, res) => {
-//   const message={
-//          
-//          
-// `        };
+
+// Define the authentication constant
+const auth = "authentication";
+
+// Create a Supabase client
+const supabaseUri = process.env.SUPABASE_URI;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUri, supabaseKey);
+
+
+async function baseHandler(req, res)
+  
+{
   
   res.send({auth:"auth success",next_uri: "https://kivtechs.cloud/dashboard/user"});
 });
+
+
+app.use(cors());
+app.all("*",baseHandler);
+
+
 
 
 
